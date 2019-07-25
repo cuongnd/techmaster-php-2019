9e1f5f172001d2f32d8f3242c6ea1e6a4f3f6f9e
cuongnd <nguyendinhcuong@gmail.com>
2019-07-25 19:14:13 +0700

    created

---
+++ giai_pt_bac_1.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <form class="phuong-trinh-b1" action="giai_pt_bac_1.php" method="post">
+        <table style="margin: 0 auto; " border="1">
+            <tr>
+                <td colspan="3">Giải phương trình bậc 1</td>
+            </tr>
+            <tr>
+                <td>phương trình</td>
+                <td><input type="text" name="a">X + </td>
+                <td><input type="text" name="b">=0</td>
+            </tr>
+            <tr>
+                <td colspan="3">Nghiệm <input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td align="center" colspan="3"><button type="submit">Xuất</button></td>
+            </tr>
+        </table>
+    </form>
+</body>
+<style type="text/css">
+    .phuong-trinh-b1{
+        font-size: 50px;
+    }
+    input{
+        padding: 20px;
+        font-size: 50px;
+    }
+    button{
+        padding: 20px;
+        font-size: 50px;
+    }
+</style>
+</html>
