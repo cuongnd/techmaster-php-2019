1bf71fa0ea2238d15020734643432fb6089bf559
cuongnd <nguyendinhcuong@gmail.com>
2019-08-19 21:00:59 +0700

    update image

--- sua_tin_tuc.php
+++ sua_tin_tuc.php
@@ -5,78 +5,104 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/styles/main_style.css">
     <title>Sửa tin tuc</title>
 </head>
 <body>
 <?php
 include_once "connect.php";
 //action sủa
-if(isset($_POST['sua']) && $_POST['sua']=="sua"){
+if (isset($_POST['sua']) && $_POST['sua'] == "sua") {
     //thuc sửa dl
-    $id=$_POST['id'];
-    $tieu_de=$_POST['tieu_de'];
-    $noi_dung_ngan=$_POST['noi_dung_ngan'];
-    $noi_dung=$_POST['noi_dung'];
-    $tieu_de=$_POST['tieu_de'];
-    $trang_thai=$_POST['trang_thai'];
-    $category_id=$_POST['category_id'];
-    $sql="UPDATE `tin_tuc` SET `category_id`='$category_id', `tieu_de` = '$tieu_de',`noi_dung_ngan` = '$noi_dung_ngan',`noi_dung` = '$noi_dung',`trang_thai` = '$trang_thai' WHERE `tin_tuc`.`id` = $id;";
-    mysqli_query($connection,$sql);
+    $id = $_POST['id'];
+    $tieu_de = $_POST['tieu_de'];
+    $noi_dung_ngan = $_POST['noi_dung_ngan'];
+    $noi_dung = $_POST['noi_dung'];
+    $tieu_de = $_POST['tieu_de'];
+    $trang_thai = $_POST['trang_thai'];
+    $category_id = $_POST['category_id'];
+
+    //update image
+    $anh_dai_dien = $_FILES["anh_dai_dien"];
+
+    $str_sql_update_image = "";
+    if ($anh_dai_dien['name']) {
+
+        $tmp_name = $anh_dai_dien['tmp_name'];
+        $name_anh_dai_dien = $anh_dai_dien['name'];
+        $new_anh_dai_dien = "anh_dai_dien_{$id}_" . $name_anh_dai_dien;
+        $image_path = "stories/images/news/image_tin_tuc/$new_anh_dai_dien";
+        move_uploaded_file($tmp_name,$image_path);
+        $str_sql_update_image = ",`anh_dai_dien`='$image_path'";
+    }
+    $sql = "UPDATE `tin_tuc` SET `category_id`='$category_id'$str_sql_update_image, `tieu_de` = '$tieu_de',`noi_dung_ngan` = '$noi_dung_ngan',`noi_dung` = '$noi_dung',`trang_thai` = '$trang_thai' WHERE `tin_tuc`.`id` = $id;";
+
+    mysqli_query($connection, $sql);
+
+
     header("location:tin_tuc.php");
 
 }
-if(isset($_GET['id']) && $_GET['id']!=0){
-    $id=$_GET['id'];
-    $sql="SELECT * FROM `tin_tuc` where id=".$id;
-    $kq=mysqli_query($connection,$sql);
-    $item=mysqli_fetch_array($kq);
+if (isset($_GET['id']) && $_GET['id'] != 0) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM `tin_tuc` where id=" . $id;
+    $kq = mysqli_query($connection, $sql);
+    $item = mysqli_fetch_array($kq);
 }
-$sql="SELECT * FROM `categories`";
-$kq=mysqli_query($connection,$sql);
+$sql = "SELECT * FROM `categories`";
+$kq = mysqli_query($connection, $sql);
 ?>
-<form action="sua_tin_tuc.php?id=<?php echo $item['id'] ?>" method="post">
-    <table style="margin: 20px auto" border="1">
-        <tr>
-            <td>Tiêu đề</td>
-            <td><input type="text" name="tieu_de" value="<?php echo $item['tieu_de'] ?>"></td>
-        </tr>
-        <tr>
-            <td>Mô tả ngắn</td>
-            <td><textarea name="noi_dung_ngan" ><?php echo $item['noi_dung_ngan'] ?></textarea></td>
-        </tr>
-        <tr>
-            <td>Nội dung</td>
-            <td><textarea name="noi_dung" ><?php echo $item['noi_dung'] ?></textarea></td>
-        </tr>
-        <tr>
-            <td>Ẩn hiện</td>
-            <td>
-                <select name="trang_thai">
-                    <option <?php echo $item['trang_thai']==1? ' selected ':'' ?>  value="1">Hiện</option>
-                    <option <?php echo $item['trang_thai']==0? ' selected ':'' ?> value="0">Ẩn</option>
-                </select>
+<div class="sua-tin-tuc">
+    <form action="sua_tin_tuc.php?id=<?php echo $item['id'] ?>" method="post" enctype="multipart/form-data">
+        <table style="margin: 20px auto" border="1">
+            <tr>
+                <td>Tiêu đề</td>
+                <td><input type="text" name="tieu_de" value="<?php echo $item['tieu_de'] ?>"></td>
+            </tr>
+            <tr>
+                <td>Ảnh đại diện</td>
+                <td><img class="anh-dai-dien" src="<?php echo $item['anh_dai_dien'] ?>"><input type="file" name="anh_dai_dien"></td>
+            </tr>
 
-            </td>
-        </tr>
-        <tr>
-            <td>Nhóm tin tức</td>
-            <td>
-                <select name="category_id">
-                    <?php while ($row=mysqli_fetch_array($kq)){ ?>
+            <tr>
+                <td>Mô tả ngắn</td>
+                <td><textarea name="noi_dung_ngan"><?php echo $item['noi_dung_ngan'] ?></textarea></td>
+            </tr>
+            <tr>
+                <td>Nội dung</td>
+                <td><textarea name="noi_dung"><?php echo $item['noi_dung'] ?></textarea></td>
+            </tr>
+            <tr>
+                <td>Ẩn hiện</td>
+                <td>
+                    <select name="trang_thai">
+                        <option <?php echo $item['trang_thai'] == 1 ? ' selected ' : '' ?> value="1">Hiện</option>
+                        <option <?php echo $item['trang_thai'] == 0 ? ' selected ' : '' ?> value="0">Ẩn</option>
+                    </select>
 
-                        <option <?php echo $item["category_id"]==$row["id"]?' selected ':'' ?> value="<?php echo $row["id"] ?>"><?php echo $row["category_name"] ?></option>
-                    <?php } ?>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <button type="submit" value="sua" name="sua">sửa</button>
-                <button type="submit" value="huy" name="huy">Hủy</button>
-            </td>
-        </tr>
-    </table>
-    <input type="hidden" name="id" value="<?php echo $item['id'] ?>">
-</form>
+                </td>
+            </tr>
+            <tr>
+                <td>Nhóm tin tức</td>
+                <td>
+                    <select name="category_id">
+                        <?php while ($row = mysqli_fetch_array($kq)) { ?>
+
+                            <option <?php echo $item["category_id"] == $row["id"] ? ' selected ' : '' ?>
+                                    value="<?php echo $row["id"] ?>"><?php echo $row["category_name"] ?></option>
+                        <?php } ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button type="submit" value="sua" name="sua">sửa</button>
+                    <button type="submit" value="huy" name="huy">Hủy</button>
+                </td>
+            </tr>
+        </table>
+        <input type="hidden" name="id" value="<?php echo $item['id'] ?>">
+    </form>
+</div>
 </body>
 </html>
--- tin_tuc.php
+++ tin_tuc.php
@@ -6,48 +6,53 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>tin tức</title>
+    <link href="assets/styles/main_style.css" rel="stylesheet">
 
 </head>
 <body>
 <?php
-$list_category=array();
+$list_category = array();
 include_once "connect.php";
-if(isset($_GET['action']) && $_GET['action']==="xoa"){
-    $id=$_GET['id'];
-    $sql="DELETE FROM `tin_tuc` WHERE id=".$id;
-    mysqli_query($connection,$sql);
+if (isset($_GET['action']) && $_GET['action'] === "xoa") {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM `tin_tuc` WHERE id=" . $id;
+    mysqli_query($connection, $sql);
     header('location:tin_tuc.php');
 }
-$sql="SELECT a_tin_tuc.*,a_categories.`id` AS category_id,a_categories.`category_name` AS category_name FROM `tin_tuc` AS a_tin_tuc LEFT JOIN `categories` AS a_categories ON a_tin_tuc.`category_id`=a_categories.`id`";
+$sql = "SELECT a_tin_tuc.*,a_categories.`id` AS category_id,a_categories.`category_name` AS category_name FROM `tin_tuc` AS a_tin_tuc LEFT JOIN `categories` AS a_categories ON a_tin_tuc.`category_id`=a_categories.`id`";
 
-$kq=mysqli_query($connection,$sql);
+$kq = mysqli_query($connection, $sql);
 //thục hiện việc đổ dữ liệu vào $list_category;
 ?>
-<table border="1" style="margin: 20px auto">
-    <thead>
-    <tr>
-        <td>tiêu đề tin tức</td>
-        <td>mô tả ngắn</td>
-        <td>Nhóm tin tức</td>
-        <td>Ẩn hiện</td>
-        <td colspan="2"><a href="them_tin_tuc.php"> Thêm</a></td>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    //while($row = mysql_fetch_array($sl))
-    ?>
-    <?php while ($row=mysqli_fetch_array($kq)){ ?>
+<div class="tin-tuc">
+    <table border="1" style="margin: 20px auto">
+        <thead>
         <tr>
-            <td><?php echo $row['tieu_de'] ?></td>
-            <td><?php echo $row['noi_dung_ngan'] ?></td>
-            <td><?php echo $row['category_name'] ?></td>
-            <td><?php echo $row['trang_thai'] ?></td>
-            <td><a href="sua_tin_tuc.php?id=<?php echo $row['id'] ?>">Sửa</a></td>
-            <td><a href="tin_tuc.php?action=xoa&id=<?php echo $row['id'] ?>">Xóa</a></td>
+            <td>tiêu đề tin tức</td>
+            <td>anh đại diện</td>
+            <td>mô tả ngắn</td>
+            <td>Nhóm tin tức</td>
+            <td>Ẩn hiện</td>
+            <td colspan="2"><a href="them_tin_tuc.php"> Thêm</a></td>
         </tr>
-    <?php } ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        <?php
+        //while($row = mysql_fetch_array($sl))
+        ?>
+        <?php while ($row = mysqli_fetch_array($kq)) { ?>
+            <tr>
+                <td><a href="sua_tin_tuc.php?id=<?php echo $row['id'] ?>"><?php echo $row['tieu_de'] ?></a></td>
+                <td><img class="anh-dai-dien" src="<?php echo $row['anh_dai_dien'] ?>"></td>
+                <td><?php echo $row['noi_dung_ngan'] ?></td>
+                <td><?php echo $row['category_name'] ?></td>
+                <td><?php echo $row['trang_thai'] ?></td>
+                <td><a href="sua_tin_tuc.php?id=<?php echo $row['id'] ?>">Sửa</a></td>
+                <td><a href="tin_tuc.php?action=xoa&id=<?php echo $row['id'] ?>">Xóa</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
