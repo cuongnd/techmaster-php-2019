9c881ebaf5d85b4a2360dbeddd0a3fe19587dc8b
cuongnd <nguyendinhcuong@gmail.com>
2019-08-19 20:26:01 +0700

    ket noi co so du lieu

--- them_tin_tuc.php
+++ them_tin_tuc.php
@@ -10,12 +10,27 @@
 <body>
 <?php
 include_once "connect.php";
+
+
 if(isset($_POST['luu'])){
     $tieu_de=$_POST['tieu_deu'];
     $mo_ta_ngan=$_POST['mo_ta_ngan'];
     $mo_ta=$_POST['mo_ta'];
     $category_id=$_POST['category_id'];
+
+
+
     $sql="INSERT INTO `tin_tuc` (`id`,`category_id`, `tieu_de`, `noi_dung_ngan`, `noi_dung`, `trang_thai`, `tin_noi_bat`, `tin_lien_quan`, `danh_gia`) VALUES (NULL,$category_id, '$tieu_de', '$mo_ta_ngan', '$mo_ta', '1', '1', '', '');";
+    mysqli_query($connection,$sql);
+    $last_id = mysqli_insert_id($connection);
+    //update image
+    $anh_dai_dien=$_FILES["anh_dai_dien"];
+    $tmp_name=$anh_dai_dien['tmp_name'];
+    $name_anh_dai_dien=$anh_dai_dien['name'];
+    $new_anh_dai_dien="anh_dai_dien_{$last_id}_".$name_anh_dai_dien;
+    $image_path="stories/images/news/image_tin_tuc/$new_anh_dai_dien";
+    move_uploaded_file($tmp_name,$image_path);
+    $sql="UPDATE `tin_tuc` SET `anh_dai_dien` = '$image_path' WHERE `tin_tuc`.`id` = $last_id;";
     mysqli_query($connection,$sql);
     header("location:tin_tuc.php");
 
@@ -25,11 +40,15 @@ $kq=mysqli_query($connection,$sql);
 
 
 ?>
-    <form action="them_tin_tuc.php" method="post">
+    <form action="them_tin_tuc.php" method="post" enctype="multipart/form-data">
         <table border="1" style="margin: 20px auto">
             <tr>
                 <td>Tiêu đề tin tức</td>
                 <td><input type="text" value="" name="tieu_de"></td>
+            </tr>
+            <tr>
+                <td>Ảnh đại diện</td>
+                <td><input type="file" name="anh_dai_dien"></td>
             </tr>
             <tr>
                 <td>Mô tả ngắn</td>
